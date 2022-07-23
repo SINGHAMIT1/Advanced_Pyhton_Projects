c93e3adc5dd66d245a16dc8a05461e75103de96a
Amit Singha <37063107+SINGHAMIT1@users.noreply.github.com>
2022-07-23 09:48:45 +0600

    Desktop Notifications App using Python

---
+++ Desktop_Notifications_app.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    while True:
+        notification.notify(
+            title="Alert !!!!",
+            message="It's already 1 hour! take break for sometime",
+            timeout=10
+        )
+        time.sleep(3600)
+# sleep() suspends execution for the given number of second
